71f4b72a60084ead582e60c60c449c06e09cd03c
NishaVyas <96906856+NishaVyas@users.noreply.github.com>
2024-01-17 13:58:27 +0530

    Add files via upload

---
+++ delete details/index.js
@@ -0,0 +1,70 @@
+function handleFormSubmit(event) {
+    event.preventDefault();
+    let myObj = {
+        username: event.target.username.value,
+        email: event.target.email.value,
+        phone: event.target.phone.value,
+    };
+    axios.post("https://crudcrud.com/api/2d405189ab864bd4a38197c0b340c1c9/appoinmentData", myObj)
+        .then((response) => {
+            console.log(response);
+            domContentLoader();
+            event.target.username.value = "";
+            event.target.email.value = "";
+            event.target.phone.value = "";
+        })
+        .catch(err => console.error(err));
+
+}
+function domContentLoader(){
+    axios.get("https://crudcrud.com/api/2d405189ab864bd4a38197c0b340c1c9/appoinmentData")
+        .then((response) => {
+            const data = response.data;
+            data.forEach((userObj)=>{
+                if(userObj.username&& userObj.email&& userObj.phone){
+                   showUserDetails(userObj);
+                }
+           });
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+}
+function showUserDetails(userObj){
+
+    const string = `${userObj.username} - ${userObj.email} - ${userObj.phone}`;
+    const list = document.querySelector('ul');
+    const newLi = document.createElement('li');
+    const newLiText = document.createTextNode(string);
+    newLi.appendChild(newLiText);
+    list.appendChild(newLi);
+
+    const deleteButton = document.createElement('button');
+    const deleteButtonText = document.createTextNode('Delete');
+    deleteButton.appendChild(deleteButtonText);
+    deleteButton.className = 'delete-button';
+    newLi.appendChild(deleteButton);
+
+    const editButton = document.createElement('button');
+    const editButtonText = document.createTextNode('Edit');
+    editButton.appendChild(editButtonText);
+    editButton.className = 'edit-button';
+    newLi.appendChild(editButton);
+
+
+    newLi.addEventListener('click',function(event){
+        if(event.target.classList.contains('delete-button')){
+            const userToDelete = event.target.parentElement;
+            userToDelete.remove();
+            axios.delete(`https://crudcrud.com/api/2d405189ab864bd4a38197c0b340c1c9/appoinmentData/${userObj._id}`)
+               .then((response) => {
+                    console.log(response);
+                })
+               .catch((err) => {
+                    console.log(err);
+                });
+        }
+    });
+    
+}
+document.addEventListener('DOMContentLoaded',domContentLoader);
